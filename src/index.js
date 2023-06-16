289d7451b35fcb1e6b040a5dc6a609a31c7954ae
katpvu <vu.kathy98@gmail.com>
2023-06-15 23:40:52 -0700

    move catStates eventlistener to line after game initialization

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ let canvas = document.getElementById('cat-state');
 canvas.width = catStates.width;
 canvas.height = catStates.height;
 const ctx = canvas.getContext('2d');
+let cat = new Cat(catStates, catSmashes)
+let game = new Game(ctx, cat)
 catStates.addEventListener('load', (e) => {
     ctx.drawImage(catStates, 0, 0, 234, 336, 355, 0, 234, 336)
 })
-let cat = new Cat(catStates, catSmashes)
-let game = new Game(ctx, cat)
 
 //Play Game button
 let playGameButtonSelector = ".play-game-button"
